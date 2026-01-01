879d462f4d9692ecf2035076724c4f43f5abde6a
AndrewM2304 <48491566+AndrewM2304@users.noreply.github.com>
2026-01-01 14:17:26 +0000

    Set Vite base to recipe-logger (#21)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const base = process.env.VITE_BASE ?? "/recipe-logger/";
+
 export default defineConfig({
-  base: "/recipe-logger/",
+  base,
   plugins: [react()],
 });
