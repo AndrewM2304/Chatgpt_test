1accaff757b78a537866a4590f54c6b8ddca783a
AndrewM2304 <48491566+AndrewM2304@users.noreply.github.com>
2026-02-06 19:34:31 +0000

    Allow scheduling directly from storage items (#176)

--- src/routes/StorageRoute.jsx
+++ src/routes/StorageRoute.jsx
@@ -4,12 +4,14 @@ export const StorageRoute = ({
   storageByLocation,
   onOpenModal,
   onUpdatePortionsLeft,
+  onScheduleItem,
 }) => {
   return (
     <StorageView
       storageByLocation={storageByLocation}
       onOpenModal={onOpenModal}
       onUpdatePortionsLeft={onUpdatePortionsLeft}
+      onScheduleItem={onScheduleItem}
     />
   );
 };
